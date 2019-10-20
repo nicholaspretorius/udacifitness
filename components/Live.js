391a10224e7d9ff32790263e92fb513f9ccf724f
Nicholas Pretorius <nicholaspretorius@gmail.com>
2019-10-20 10:18:50 +0200

    feat: Add granted outline

--- components/Live.js
+++ components/Live.js
@@ -6,7 +6,7 @@ import { purple, white } from "./../utils/colors";
 class Live extends Component {
   state = {
     coords: null,
-    status: "undetermined",
+    status: "granted",
     direction: ""
   };
 
@@ -23,8 +23,12 @@ class Live extends Component {
 
     if (status === "denied") {
       return (
-        <View>
-          <Text>Denied</Text>
+        <View style={styles.center}>
+          <Foundation name="alert" size={50} />
+          <Text>
+            Unfortunately, this page requires location permission access which you have denied. You
+            can enable this by visiting your settings and enabling location service for this app.
+          </Text>
         </View>
       );
     }
@@ -35,7 +39,7 @@ class Live extends Component {
           <Foundation name="alert" size={50} />
           <Text>Please enable location permissions to view this page.</Text>
           <TouchableOpacity onPress={this.askPermission} style={styles.button}>
-            <Text style={style.buttonText}>Enable</Text>
+            <Text style={styles.buttonText}>Enable</Text>
           </TouchableOpacity>
         </View>
       );
@@ -43,8 +47,20 @@ class Live extends Component {
 
     return (
       <View>
-        <Text>Live</Text>
-        <Text>{JSON.stringify(this.state)}</Text>
+        <View style={styles.directionContainer}>
+          <Text style={styles.header}>You're heading</Text>
+          <Text style={styles.direction}>North</Text>
+        </View>
+        <View style={styles.metricContainer}>
+          <View style={styles.metric}>
+            <Text style={[styles.header, { color: white }]}>Altitude</Text>
+            <Text style={[styles.subHeader, { color: white }]}>{200} feet</Text>
+          </View>
+          <View style={styles.metric}>
+            <Text style={[styles.header, { color: white }]}>Speed</Text>
+            <Text style={[styles.subHeader, { color: white }]}>{300} MPH</Text>
+          </View>
+        </View>
       </View>
     );
   }
@@ -72,6 +88,39 @@ const styles = StyleSheet.create({
   buttonText: {
     color: white,
     fontSize: 20
+  },
+  directionContainer: {
+    flex: 1,
+    justifyContent: "center"
+  },
+  header: {
+    fontSize: 35,
+    textAlign: "center"
+  },
+  direction: {
+    color: purple,
+    fontSize: 120,
+    textAlign: "center"
+  },
+  metricContainer: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    backgroundColor: purple
+  },
+  metric: {
+    flex: 1,
+    paddingTop: 15,
+    paddingBottom: 15,
+    backgroundColor: "rgba(255, 255, 255, 0.1)",
+    marginTop: 20,
+    marginBottom: 20,
+    marginLeft: 10,
+    marginRight: 10
+  },
+  subHeader: {
+    fontSize: 25,
+    textAlign: "center",
+    marginTop: 5
   }
 });
 
